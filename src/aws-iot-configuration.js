06d99d85519f85084a6d4a66979256b30e6e8fb7
Enrique Othon <enrique.othon@tylertech.com>
2020-08-10 14:01:37 -0500

    change

--- src/aws-iot-configuration.js
+++ src/aws-iot-configuration.js
@@ -1,7 +1,7 @@
 var awsIotConfiguration = {
     endpoint: 'wss://a3en7mwxss7zog-ats.iot.us-west-2.amazonaws.com/mqtt', 
     region: 'us-west-2',
-    poolId: 'us-west-2_P5SI3MJdL',
+    poolId: 'us-west-2_gaMzvjWCF',
     host: 'a3en7mwxss7zog-ats.iot.us-west-2.amazonaws.com'
   };
  module.exports = awsIotConfiguration;
